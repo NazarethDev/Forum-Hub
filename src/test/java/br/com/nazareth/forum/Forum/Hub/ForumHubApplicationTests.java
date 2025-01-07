c8e8defeddffc48e0070551dbee0f4668df0b849
NazarethDev <lorran.nazareth@gmail.com>
2025-01-07 17:20:21 -0300

    Inicial commit

---
+++ src/test/java/br/com/nazareth/forum/Forum/Hub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nazareth.forum.Forum.Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
